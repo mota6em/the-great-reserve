57b37938594e6a51f20f67489a5d12ef85907217
motasem abubaraka <mota6em@gmail.com>
2025-03-18 02:28:34 +0100

    Added SceneLoader.cs and implemented scene transitions for 'Vissza' button. Updated LoadGameScene UI and adjusted image assets.

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("LoadGameScene");
+
+    }
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+}
