df3b1092578902747999ae529664a7e00d081f2e
Jármezei Ferenc <ferencjarmezei@gmail.com>
2025-04-03 21:36:04 +0200

    Animals sometimes stops to take a break from moving, animals now moves within GameArea

--- Assets/Scripts/Animals/Lion.cs
+++ Assets/Scripts/Animals/Lion.cs
@@ -5,6 +5,12 @@ public class Lion : Carnivore
 {
     protected override void InitializeAnimal()
     {
-        moveSpeed = 3.0f;
+        moveSpeed = 2.0f;
+        noiseOffsetX = Random.Range(0f, 100f);
+        noiseOffsetY = Random.Range(0f, 100f);
+        standTimer = 0f;
+        standDuration = 5f;
+        timeBetweenStopsTimer = 0f;
+        timeBetweenStops = 7f;
     }
 }
